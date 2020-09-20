aba005360be9b7eac5470668a64024ef2f05f060
jfobrycki <56492823+jfobrycki@users.noreply.github.com>
2020-09-20 17:02:31 -0400

    initial map testing

---
+++ update_map.js
@@ -0,0 +1,38 @@
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A new value was submitted: ' + this.state.value);
+    event.preventDefault();
+    pointsCoords.geometry.value.push(Number(this.state.value))
+    newMapping(pointsCoords);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Value: 
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(
+  <NameForm />,
+  document.getElementById('pointValue')
+);
